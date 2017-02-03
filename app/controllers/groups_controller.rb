20b2173e3483274cd7ae3a10aa935f94873b56e4
wangyanli <wylhope365@163.com>
2017-02-03 21:52:43 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,11 @@ end
 
 def create
   @group = Group.new(group_params)
-  @group.save
-
-   redirect_to groups_path
+  if @group.save
+    redirect_to groups_path
+  else
+    render :new
+  end
  end
 
  def update
@@ -34,7 +36,7 @@ def create
    flash[:alert] = "Group deleted"
    redirect_to groups_path
  end
- 
+
  private
   def group_params
     params.require(:group).permit(:title, :description)
